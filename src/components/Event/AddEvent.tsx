968a71006ace1aa87e99d229633aab89c1575535
Ali Osman Yildirim <aosmanyildirim@gmail.com>
2023-12-06 10:03:43 +0100

    add Events style

--- src/components/Event/AddEvent.tsx
+++ src/components/Event/AddEvent.tsx
@@ -15,7 +15,7 @@ const AddEvent: React.FC<addEventType> = ({closeModal,addEvent,setEventList}) =>
     startdatum: "",
     enddatum: "",
     adresse: '',
-    plz: 0,
+    plz: 8850,
     ort: '',
     link: '',
     image: ''
@@ -39,43 +39,46 @@ const AddEvent: React.FC<addEventType> = ({closeModal,addEvent,setEventList}) =>
 
   return (
     <div className='form-main-container'>
-      <div className='login-register-container'>
+      <div className='add-event-container'>
         <h2>Add Event</h2>
-        <div className='form-input-element'>
+        <div className="add-event-input-container">
+        <div className='event-input-element'>
           <label>Name:</label>
           <input type="text" value={values.name} onChange={e => setValue({ ...values, name: e.target.value })} />
         </div>
-        <div className='form-input-element'>
+        <div className='event-input-element'>
           <label>Start Date:</label>
-          <input type="text" value={values.startdatum} onChange={e => setValue({ ...values, startdatum: e.target.value })} />
+          <input type="date" value={values.startdatum} onChange={e => setValue({ ...values, startdatum: e.target.value })} />
         </div>
-        <div className='form-input-element'>
+        <div className='event-input-element'>
           <label>End Date:</label>
-          <input type="text" value={values.enddatum} onChange={e => setValue({ ...values, enddatum: e.target.value })} />
+          <input type="date" value={values.enddatum} onChange={e => setValue({ ...values, enddatum: e.target.value })} />
         </div>
-        <div className='form-input-element'>
+        <div className='event-input-element'>
           <label>Adresse:</label>
           <input type="text" value={values.adresse} onChange={e => setValue({ ...values, adresse: e.target.value })} />
         </div>
-        <div className='form-input-element'>
+        <div className='event-input-element'>
           <label>PLZ:</label>
           <input type="number" value={values.plz} onChange={e => setValue({ ...values, plz: parseInt(e.target.value, 10) })} />
         </div>
-        <div className='form-input-element'>
+        <div className='event-input-element'>
           <label>Ort:</label>
           <input type="text" value={values.ort} onChange={e => setValue({ ...values, ort: e.target.value })} />
         </div>
-        <div className='form-input-element'>
+        <div className='event-input-element'>
           <label>Link:</label>
           <input type="text" value={values.link} onChange={e => setValue({ ...values, link: e.target.value })} />
         </div>
-        <div className='form-input-element'>
+        <div className='event-input-element'>
           <label>Image:</label>
           <input type="text" value={values.image} onChange={e => setValue({ ...values, image: e.target.value })} />
         </div>
         {error && <p style={{ color: 'red' }}>{error}</p>}
         <button className='form-button' onClick={handleAddEvent}>Add Event</button>
-        <button className='form-button form-close-button' onClick={closeModal}>Close</button>
+        <button className='form-button form-close-button' onClick={closeModal}>Close</button>      
+        </div>
+       
       </div>
     </div>
   );
