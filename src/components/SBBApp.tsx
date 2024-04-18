4e2ddd69668de05302443070d89b7d8481b190d7
Ali Osman YILDIRIM <aosmanyildirim@gmail.com>
2024-04-18 13:54:44 +0200

    Add files via upload

--- src/components/SBBApp.tsx
+++ src/components/SBBApp.tsx
@@ -32,48 +32,45 @@ const SBBApp = () => {
   };
 
   return (
-   <div className='col-12 col-md-8'>
-    <img className="sbb-img" alt="sbb logo" src={sbbLogo}/>
+    <div className='col-12 col-md-8'>
+      <img className="sbb-img" alt="sbb logo" src={sbbLogo}/>
       <form onSubmit={callSBB} id="sbbapp">
         {
           <div>
-              <div className='sbb-form-container ms-2'>
-                <div className='sbb-form-label-container'>
-                  <label className="sbb-form-label">Von:</label>
-                  <input id="from" type="text" name="from" placeholder="Ort" 
-                      className="sbb-form-input" onChange={e => setFrom(e.target.value)}/>
-                </div>
-                <div className='sbb-form-label-container ms-2'>
-                  <label className="sbb-form-label">Nach:</label>
-                    <input id="to" type="text" name="to" className="sbb-form-input" 
-                        onChange={e => setTo(e.target.value)}/>
-                </div>      
+            <div className='sbb-form-container ms-2'>
+              <div className='sbb-form-label-container'>
+                <label className="sbb-form-label">Von:</label>
+                <input id="from" type="text" name="from" placeholder="Ort" className="sbb-form-input" onChange={e => setFrom(e.target.value)}/>
               </div>
-                <div className='sbb-form-container ms-2'>
-                  <div className='sbb-form-label-container'>
-                    <label className="sbb-form-label">Datum:</label>
-                    <input id="date" type="date" name="date"
-                        className="sbb-form-input" onChange={e => setDate(e.target.value)} />
-                  </div>
-                  <div className='sbb-form-label-container'>
-                    <label className="sbb-form-label">Zeit:</label>
-                    <input id="time" type="time" name="time"  
-                      className="sbb-form-input" onChange={e => setTime(e.target.value)} />
-                  </div>      
+              <div className='sbb-form-label-container ms-2'>
+                <label className="sbb-form-label">Nach:</label>
+                <input id="to" type="text" name="to" className="sbb-form-input" onChange={e => setTo(e.target.value)}/>
+              </div>      
+            </div>
+            <div className='sbb-form-container ms-2'>
+                <div className='sbb-form-label-container'>
+                  <label className="sbb-form-label">Datum:</label>
+                  <input id="date" type="date" name="date"
+                      className="sbb-form-input" onChange={e => setDate(e.target.value)} />
                 </div>
-                <div className="sbb-form-container ">
-                  <div>
-                    <input type="radio" value="false" name="isArrival" checked id="departure" className='ms-2'/>
-                    <label htmlFor="departure" className="sbb-form-label-abfahrt ms-2">Abfahrt</label>
-                    <input type="radio" value="true" name="isArrival" id="arrival" className="is_arrival ms-2"/>
-                    <label htmlFor="arrival" className='ms-2'>Ankunft</label>
-                  </div>     
-                </div>
-                <div className="sbb-form-container ms-2">
-                  <div>
-                    <input type="submit" value="Verbindung suchen" id="search--sbb" 
-                    className='sbb-search button'/>
-                  </div>      
+                <div className='sbb-form-label-container'>
+                  <label className="sbb-form-label">Zeit:</label>
+                  <input id="time" type="time" name="time"  
+                    className="sbb-form-input" onChange={e => setTime(e.target.value)} />
+                </div>      
+            </div>
+            <div className="sbb-form-container ">
+              <div>
+                <input type="radio" value="false" name="isArrival" id="departure" className='ms-2 ' />
+                <label htmlFor="departure" className="sbb-form-label-abfahrt ms-2">Abfahrt</label>
+                <input type="radio" value="true" name="isArrival" id="arrival" className="is_arrival ms-2"/>
+                <label htmlFor="arrival" className='ms-2'>Ankunft</label>
+              </div>     
+            </div>
+            <div className="sbb-form-container ms-2">
+              <div>
+                <input type="submit" value="Verbindung suchen" id="search--sbb" className='sbb-search button'/>
+              </div>      
             </div>
           </div>    
         }
